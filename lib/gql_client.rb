627710a0c79598798ffffe637f19c7e2d78064e4
lezwon <lezwon@synup.com>
2018-12-27 10:43:29 +0530

    added introspection query

--- lib/gql_client.rb
+++ lib/gql_client.rb
@@ -5,7 +5,6 @@ require 'json'
 
 
 module GqlClient
-  # TODO: static method for query
   def self.execute(url, query, headers, variables = {})
     url = URI(url)
     http = Net::HTTP.new(url.host, url.port)
@@ -30,6 +29,7 @@ module GqlClient
     attr_accessor :request
     attr_accessor :http
 
+    INTROSPECTION_QUERY_FILE = "introspection_query.graphql"
 
     def initialize(url, headers)
       url = URI(url)
@@ -39,6 +39,19 @@ module GqlClient
       @request["cookie"] = 'request_method=POST'
       @request["content-type"] = 'application/json'
     end
+
+    def schema
+      @schema ||= begin
+        request.body = {"query": introspection_query }.to_json
+        response = http.request(request)
+        raise StandardError, "Introspection Query Failed" unless response.code == "200"
+        JSON.parse(response.read_body)
+      end
+    end
+
+    def introspection_query
+      File.read(INTROSPECTION_QUERY_FILE)
+    end 
 
     def execute(query, variables = {})
       body = {
